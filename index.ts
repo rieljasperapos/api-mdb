a4a9d8ea1b63107398c9df03b805082d74d69ff9
Riel <jasper.apos@gmail.com>
2024-02-02 00:12:06 +0800

    cors update

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://mdb-five.vercel.app"],
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true
 }));
